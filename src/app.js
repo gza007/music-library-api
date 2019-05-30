686ffbd0da426e577de1d895c0b9a9194cf0f93c
Geevan Patel <geevan.peter.patel@gmail.com>
2019-05-30 01:04:13 +0100

    added src/models/artist and ../album files

--- src/app.js
+++ src/app.js
@@ -2,8 +2,8 @@ const express = require('express');
 
 const app = express();
 
-app.get('*', (req, res) => {
-  res.status(200).json({ message: 'Hello World!' });
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello YOU World!' });
 });
 
 module.exports = app;
