55bc8934697369727b183d16ad134e31fa36ce6a
Geevan Patel <geevan.peter.patel@gmail.com>
2019-06-05 00:52:12 +0100

    passing first artist test

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+const artistRouter = require('./routes/artists');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello YOU World!' });
-});
+app.use(express.json());
+app.use('/artists', artistRouter);
+
 
 module.exports = app;
