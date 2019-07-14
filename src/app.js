b7e2cfecf3e5f5a994df57c543224b1073471fce
Geevan Patel <geevan.peter.patel@gmail.com>
2019-07-14 11:32:52 +0100

    added cors middleware, and ran npm audit fix

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 
 const artistRouter = require('./routes/artists');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use('/artists', artistRouter);
